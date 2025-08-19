321b97910078c0a7a8ee94c3108f6c989ca3e7c1
Mannathan <mannathands@gmail.com>
2025-08-19 12:05:11 +0530

    Update main.tf

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -46,6 +46,10 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this.id
   }
+  route {
+    cidr_block = "10.100.0.0/16"
+    gateway_id = aws_internet_gateway.this.id
+  }
 
   tags = {
     Name = "${var.vpc_name}-public-rt"
